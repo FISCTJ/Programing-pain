cef04ebb515557d75b9ad6ffbbb061e37ef57a35
Thomas Fischer (1000117937) <FISCTJ1@student.op.ac.nz>
2024-06-18 13:47:41 +1200

    Lab 19

---
+++ Lab 19/Finding largest entry/Finding largest entry/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Finding_largest_entry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second, third;
+            first()second()third();
+            Console.WriteLine();
+            do
+            {
+                if (first > second)
+                {
+                    Console.WriteLine("The first is greator than second");
+                }
+                if (second < third)
+                {
+                    Console.WriteLine("second is less than third");
+                }
+            } while ("");
+        }
+        static int first()
+        {
+            Console.WriteLine("Enter the first number");
+            first = Console.ReadLine();
+        }
+        static int second()
+        {
+            Console.WriteLine("Enter the Second number");
+            second = Console.ReadLine();
+        }
+        static int third()
+        {
+            Console.WriteLine("Enter the third number");
+            third = Console.ReadLine();
+        }
+    }
+}
---
+++ Lab 19/Value return methods/Value return methods/Program.cs
@@ -0,0 +1,50 @@
+﻿/************************
+ * title value methods
+ * by Thomas Fischer
+ * Date 21/05/2024
+ ***********************/
+namespace Value_return_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp;
+            int usr;
+            Console.WriteLine("Enter number to be altered");
+            temp = Console.ReadLine();
+            usr = Convert.ToInt32(temp);
+            Console.WriteLine(square(usr));
+            Console.WriteLine(cube(usr));
+            Console.WriteLine(twotimes(usr));
+            Console.WriteLine(half(usr));
+            Console.ReadLine();
+        }
+
+        static int square(int num)
+        {
+            int square;
+            square = num * num * num * num;
+            return square;
+        }
+        static int cube(int num)
+         {
+             int cube;
+             cube = num * num * num;
+             return cube;
+
+         }
+         static int twotimes(int num)
+         {
+             int twotimes;
+             twotimes = num * 2;
+             return twotimes;
+         }
+         static double half(int num)
+         {
+             double half;
+             half = num * 0.5;
+             return half;
+         }
+    }
+}
