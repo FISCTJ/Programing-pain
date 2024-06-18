5454244ee1eed50376fc62124bfcbb2ac042cdbd
Thomas Fischer (1000117937) <FISCTJ1@student.op.ac.nz>
2024-06-18 13:53:02 +1200

    Lab 21

---
+++ Lab 21/About Contestant ()/About Contestant ()/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace About_Contestant___
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student[]gameshow= New contestant[4];
+            Paddright right(int c = i + 4; i < gameshow.Length; i++);
+        }
+    }
+}
---
+++ Lab 21/Milkbar Icecream managment/Milkbar Icecream managment/Program.cs
@@ -0,0 +1,35 @@
+﻿/************************************
+ * title Milk-bar management
+ * by Thomas Fischer
+ * Date 28/05/2024
+ ************************************/
+namespace Milkbar_Icecream_managment
+{
+    public struct icecreams
+    {
+        public string name, flavour;
+        public double cost; 
+        public int number;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            icecreams frozencream;
+            Console.WriteLine("What icecream would you like");
+            frozencream.name = Console.ReadLine();
+            frozencream.flavour = Console.ReadLine();
+            frozencream.cost = 30;
+            frozencream.number = 3;
+            Console.WriteLine($"{frozencream.name} {frozencream.flavour} {frozencream.cost} {frozencream.number}");
+            icecreams[] icetypes = new icecreams[5];
+            icetypes[0].name = "Choc bar";
+            icetypes[0].flavour = "one";
+            icetypes[0].cost = 3;
+            icetypes[0].number = 1;
+            icetypes[1].name = "2" ;
+            Console.ReadLine();
+        }
+        
+    }
+}
