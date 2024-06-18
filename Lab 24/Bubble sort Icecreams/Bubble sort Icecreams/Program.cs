a3b98ec49d0b8dfe06490d22be73898ba43ebdb7
Thomas Fischer (1000117937) <FISCTJ1@student.op.ac.nz>
2024-06-18 13:57:20 +1200

    Kab 24

---
+++ Lab 24/Bubble sort Icecreams/Bubble sort Icecreams/Program.cs
@@ -0,0 +1,37 @@
+﻿/******************************
+ * Title bubble sort icecreams
+ * by Thomas Fischer
+ * date 11/06/2024
+ ******************************/
+namespace Bubble_sort_Icecreams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aLine;
+            int numbers;
+            StreamReader str = new StreamReader(@"icecream.txt");
+            while (!str.EndOfStream)
+            {
+                aLine = str.ReadLine();
+                Console.WriteLine(aLine);
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int pos = 0; pos < numbers.Length - 1; pos++)
+                {
+                    if (numbers[pos + 1] < numbers[pos])
+                    {
+                        int temp1 = numbers[pos + 1];
+                        numbers[pos + 1] = numbers[pos];
+                        numbers[pos] = temp1;
+                    }
+                }
+                Console.WriteLine(numbers[i]);
+            }
+            str.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab 24/Numbers array Bubble/Numbers array Bubble/Program.cs
@@ -0,0 +1,35 @@
+﻿/**********************************
+ * Title Numbers array bubble sort
+ * by Thomas Fischer
+ * date 11/06/2024
+ **********************************/
+namespace Numbers_array_Bubble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp;
+            int[] numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter number");
+                temp = Console.ReadLine();
+                numbers[i]= Convert.ToInt32(temp);
+            }
+            for  (int i = 0; i < numbers.Length; i++)
+            {
+                for (int pos = 0; pos < numbers.Length - 1; pos++)
+                {
+                    if (numbers[pos + 1] < numbers[pos])
+                    {
+                        int temp1 = numbers[pos + 1];
+                        numbers[pos + 1] = numbers[pos];
+                        numbers[pos] = temp1;
+                    }
+                }
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
